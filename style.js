53444b871a1db4bf4b36d46cdc7c417efeafc271
Ahonmisi Moses <79580586+Sidmoses73@users.noreply.github.com>
2023-11-04 16:06:44 +0100

    Create style.js

---
+++ style.js
@@ -0,0 +1,15 @@
+function setProgress(progressId, percentage) {
+        var progressElement = document.getElementById(progressId);
+        progressElement.style.width = percentage + '%';
+}
+
+setProgress('progress1', 95);
+setProgress('progress2', 80);
+setProgress('progress3', 65);
+setProgress('progress4', 65);
+setProgress('progress5', 60);
+setProgress('progress6', 60);
+setProgress('progress7', 58);
+setProgress('progress8', 50);
+setProgress('progress9', 90);
+setProgress('progress10', 89);
